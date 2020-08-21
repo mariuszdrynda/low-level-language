b9811162273f9a9bc74a56ff1a8670f97eaeb975
Mariusz Drynda <mariuszdrynda@gmail.com>
2020-08-22 00:37:02 +0200

    Simple grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[macro_use] extern crate lalrpop_util;
+lalrpop_mod!(pub grammar);
 fn main() {
-    println!("Hello, world!");
+    ()
+}
+#[test]
+fn lall_grammar_test() {
+    assert!(grammar::TopParser::new().parse("22").is_ok());
+    assert!(grammar::TopParser::new().parse("(22)").is_ok());
+    assert!(grammar::TopParser::new().parse("((((22))))").is_ok());
+    assert!(grammar::TopParser::new().parse("((22)").is_err());
 }
